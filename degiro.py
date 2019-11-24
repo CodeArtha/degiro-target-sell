eafef76bdc724d9c8d401502db6fe415189340cc
codeartha <codeartha@gmail.com>
2019-11-24 09:44:05 +0100

    WIP adding sell orders and canceling existing ones

--- degiro.py
+++ degiro.py
@@ -38,3 +38,44 @@ class Degiro:
             disconnect_button = self.browser.find_element_by_text("Déconnexion")
             disconnect_button.click()
             self.browser.Close()
+
+    def add_sell_order(self, qty, price1, price2, ordertype, **kwargs):
+        # ordertype (1 = limit, 2 = stoploss, 3 = stoplimit)
+
+        # Dealing with optional arguments
+        name = kwargs.get('name', None)
+        ticker = kwargs.get('ticker', None)
+
+        # Input sanitation (overly paranoid but we're dealing with real money here)
+        qty = int(qty)
+        price1 = float(price1)
+        price2 = float(price2)
+        ordertype = int(ordertype)
+        if name is None and ticker is None:
+            raise ValueError("Name and ticker cannot both be None")
+        if qty <= 0:
+            raise ValueError("Can't sell less than 1 stock")
+        if price1 <= 0:
+            raise ValueError("Can't have negative or null sell price")
+        if ordertype not in (1, 2, 3):
+            raise TypeError("Invalid order type")
+        if ordertype == 3 and (price2 <= 0 or price2 < price1):
+            raise ValueError("Stop limit price should be smaller than stop loss price.")
+
+        # Handeling the case where we don't have the stock's name
+        if name is None:
+            name = self.find_ticker_name(ticker)
+
+        # First we cancel all orders for that stock
+        # Then we set a new sell order for that stock
+        self.cancel_orders(name)
+
+
+    def find_ticker_name(self, ticker):
+        # first searches in database if the ticker already has a name association
+        # then checks on degiro's website
+        pass
+
+    def cancel_orders(self, name):
+        # finds all orders open on a "named" stock and cancels them
+        pass
